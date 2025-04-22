43faa8a6f89f4f21b3fdcdd28e9c7af93047270f
rohandashrd <ddash098@gmail.com>
2025-04-22 17:20:27 +0530

    added validation for user

--- activity-service/src/main/java/com/springboot/activityservice/service/ActivityService.java
+++ activity-service/src/main/java/com/springboot/activityservice/service/ActivityService.java
@@ -15,9 +15,15 @@ import java.util.stream.Collectors;
 public class ActivityService {
 
     private final ActivityRepository activityRepository;
+    private final UserValidationService userValidationService;
 
     public ActivityResponse trackActivity(ActivityRequest request) {
 
+
+        boolean isValidUser = userValidationService.validateUser(request.getUserId());
+        if (!isValidUser) {
+            throw new RuntimeException("Invalid user: "+request.getUserId());
+        }
         Activity activity = Activity.builder()
                 .userId(request.getUserId())
                 .activityType(request.getActivityType())
--- user-service/src/main/java/com/springboot/userservice/service/UserService.java
+++ user-service/src/main/java/com/springboot/userservice/service/UserService.java
@@ -53,4 +53,8 @@ public class UserService {
         userResponse.setUpdatedAt(user.getUpdatedAt());
         return userResponse;
     }
+
+    public Boolean existByUserId(String userId) {
+        return userRepository.existsById(userId);
+    }
 }
