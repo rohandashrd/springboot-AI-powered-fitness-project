fb9276ab322b88ca9b0e256e177944e6b496aece
rohandashrd <ddash098@gmail.com>
2025-05-02 18:00:13 +0530

    added activity dashboard

--- fitness-app-frontend/src/App.jsx
+++ fitness-app-frontend/src/App.jsx
@@ -1,35 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Button } from "@mui/material";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "react-oauth2-code-pkce";
+import { useDispatch } from "react-redux";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+  useLocation,
+} from "react-router";
+import { setCredentials } from "./store/authSlice";
+import ActivityForm from "./components/ActivityForm";
+import ActivityList from "./components/ActivityList";
+import ActivityDetail from "./components/ActivityDetail";
 
-function App() {
-  const [count, setCount] = useState(0)
+const ActivitiesPage = () => {
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+    <ActivityForm onActivitiesAdded={() => window.location.reload()} />
+    <ActivityList />
+  </Box>);
+};
+
+function App() {
+  const { token, tokenData, logIn, logOut, isAuthenicated } =
+    useContext(AuthContext);
+  const dispatch = useDispatch();
+  const [authReady, setAuthReady] = useState(false);
+
+  useEffect(() => {
+    if (token) {
+      dispatch(setCredentials({ token, user: tokenData }));
+      setAuthReady(true);
+    }
+  }, [token, tokenData, dispatch]);
+
+  return (
+    <Router>
+      {!token ? (
+        <Button
+          variant="contained"
+          color="#dc004e"
+          onClick={() => {
+            logIn();
+          }}
+        >
+          Login
+        </Button>
+      ) : (
+        // <pre>{JSON.stringify(tokenData, null, 2)}</pre>
+
+        <Box component="section" sx={{ p: 2, border: "1px dashed grey" }}>
+          <Routes>
+            <Route path="/activities" element={<ActivitiesPage />} />
+            <Route path="/activities/:id" element={ActivityDetail} />
+
+            <Route
+              path="/"
+              element={
+                token ? (
+                  <Navigate to="/activities" replace />
+                ) : (
+                  <div>Welcome! Please Log In</div>
+                )
+              }
+            />
+          </Routes>
+        </Box>
+      )}
+    </Router>
+  );
 }
 
-export default App
+export default App;
